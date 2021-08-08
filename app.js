e4ad0fa7b8ad4a74f5f0685ad9ab652b9cb2a433
sophiehsu0213 <52458821+sophiehsu0213@users.noreply.github.com>
2021-08-09 00:14:04 +0800

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-app = express()const linebot = require('linebot');// 判別開發環境
+const app = express();
+const linebot = require('linebot');// 判別開發環境
 if (process.env.NODE_ENV !== 'production') {      // 如果不是 production 模式
  require('dotenv').config()                      // 使用 dotenv 讀取 .env 檔案
 }const bot = linebot({
