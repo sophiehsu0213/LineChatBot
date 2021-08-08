c4cba33ba1f21dc4d60497de404194e962d80ad7
sophiehsu0213 <52458821+sophiehsu0213@users.noreply.github.com>
2021-08-09 02:39:46 +0800

    bynmfrvr

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const linebotParser = bot.parser();bot.on('message', function (event) {
  if(findWords('你')){
    event.reply('你是誰?我是誰?誰又知道呢?')
  }else{
-   event.reply(event.message.text);
+   event.reply('無法辨識');
  }
  /*
  switch (event.message.text) {
