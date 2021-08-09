939611f1817f57f076bb98595795fb52b586e8f8
sophiehsu0213 <52458821+sophiehsu0213@users.noreply.github.com>
2021-08-09 20:43:20 +0800

    bynm

--- app.js
+++ app.js
@@ -50,38 +50,6 @@ const linebotParser = bot.parser();bot.on('message', function (event) {
  }else{
    event.reply('偶不知道你在說啥，不過你可以繼續說:)')
  }
- /*
- switch (event.message.text) {
-   case '你':
-     event.reply('你是誰?我是誰?誰又知道呢?')
-     break
-   case 'hi':
-     event.reply('hi!很高興看到你!來跟我聊天吧!')
-     break
-   case 'hello':
-     event.reply('哈囉!很高興看到你!來跟我聊天吧!')
-     break
-   case '天氣':
-     event.reply('今天天氣依舊如此美好，就如同我的心情:)')
-     break
-   case '聽說':
-     event.reply('什麼????')
-     break
-   case '問':
-     event.reply('駭客都是自學的，自己去查')
-     break
-   case '說':
-     event.reply('說什麼說，不要再說了，等吃飽再說吧！')
-     break
-   case '名字':
-     event.reply('我是河豚，你叫什麼?')
-     break
-   case '排球':
-     event.reply('一萬超愛排球少年！')
-     break
-   default:
-     event.reply('偶不知道你在說啥，不過你可以繼續說:)')
- }*/
 });app.post('/', linebotParser);app.listen(process.env.PORT || 3000, () => {
  console.log('Express server start')
 });
